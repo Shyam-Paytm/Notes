1fdf7a27892edd9761c6c4b7ddc114f0d63002b8
Shyam Singhania <shyam.singhania@paytm.com>
2022-09-05 10:27:43 +0530

    Add Worker to backup notes of user in Firestore

---
+++ app/src/main/java/com/example/notes/worker/NotesWorker.kt
@@ -0,0 +1,16 @@
+package com.example.notes.worker
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.notes.repositories.NoteRepository
+import com.example.notes.roomdb.NoteDB
+
+class NotesWorker(private val context: Context, private val params: WorkerParameters) :
+    Worker(context, params) {
+    override fun doWork(): Result {
+        val noteRepository = NoteRepository(NoteDB.getInstance(context).getNoteDao())
+        noteRepository.backupNotesInFirestore()
+        return Result.success()
+    }
+}
